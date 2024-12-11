85d3f39c4eb858b83dcbd296bf0ab7a5c72569b3
lopplopp <michaelpoonhowan@hotmail.com>
2024-12-11 16:10:24 +0800

    fix basename issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import GlobalState from './context/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/anime-vault'>
     {/* <StrictMode> */}
       <GlobalState>
         <App />
